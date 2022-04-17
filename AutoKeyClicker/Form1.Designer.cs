f45d8bda744ea194f7a0362fa13fba098054f385
Johku <101747915+JohkunPaja@users.noreply.github.com>
2022-04-17 18:38:52 +0300

    Add files via upload

---
+++ AutoKeyClicker/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace AutoKeyClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Spam = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Delay = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.GtMod = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key pressed:";
+            // 
+            // Spam
+            // 
+            this.Spam.Location = new System.Drawing.Point(118, 9);
+            this.Spam.Name = "Spam";
+            this.Spam.Size = new System.Drawing.Size(156, 20);
+            this.Spam.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Interval (ms):";
+            // 
+            // Delay
+            // 
+            this.Delay.Location = new System.Drawing.Point(118, 35);
+            this.Delay.Name = "Delay";
+            this.Delay.Size = new System.Drawing.Size(156, 20);
+            this.Delay.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(118, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Start (F3)";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(199, 61);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 20);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Stop (F4)";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // GtMod
+            // 
+            this.GtMod.AutoSize = true;
+            this.GtMod.BackColor = System.Drawing.Color.Transparent;
+            this.GtMod.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GtMod.ForeColor = System.Drawing.Color.Red;
+            this.GtMod.Location = new System.Drawing.Point(16, 60);
+            this.GtMod.Name = "GtMod";
+            this.GtMod.Size = new System.Drawing.Size(81, 21);
+            this.GtMod.TabIndex = 8;
+            this.GtMod.Text = "Gt mode";
+            this.GtMod.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(290, 96);
+            this.Controls.Add(this.GtMod);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Delay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Spam);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AutoKeyClicker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Hotkey);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Spam;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Delay;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox GtMod;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AutoKeyClicker/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
+
+
+namespace AutoKeyClicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Hotkey(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F3)
+            {
+                button3.PerformClick();
+            }
+                                          // You can edit this to make it better or change the hotkeys!
+            if (e.KeyCode == Keys.F4)
+            {
+                button4.PerformClick();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+            string message = "Do you want to join my discord server?";
+            string title = "Discord server";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result = MessageBox.Show(message, title, buttons);
+            if (result == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("https://discord.gg/sCHV2q9p");
+            }
+            else
+            {
+                //pass 
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(GtMod.Checked)
+            {
+                int del1 = 0;
+                int.TryParse(Delay.Text, out del1);
+                SendKeys.Send("{Enter}");
+                SendKeys.Send(Spam.Text);
+                SendKeys.Send("{Enter}");
+                Thread.Sleep(del1);
+                return;
+            }
+            int del = 0;
+            int.TryParse(Delay.Text, out del);
+            SendKeys.Send(Spam.Text);
+            Thread.Sleep(del);
+            return;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
